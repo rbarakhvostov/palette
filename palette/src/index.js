e30e0a277ece5e334663137dd2630b07ebf887c0
Roman Barakhvostov <rbarakhvostov@gmail.com>
2019-04-28 03:16:19 +0300

    feat: add transform tool

--- palette/src/index.js
+++ palette/src/index.js
@@ -5,18 +5,40 @@ function selectTool(event) {
       for (let i = 0, l = toolMenu.children.length; i < l; i++) {
         if (toolMenu.children[i].classList.contains('selected-tool')) {
           toolMenu.children[i].classList.remove('selected-tool');
+          /*toolState[toolMenu.children[i].dataset.name] = 0;*/
         }
       }
       target.classList.add('selected-tool');
+      /*toolState[target.dataset.name] = 1;*/
       return;
     }
     target = target.parentNode;
   }
 }
 
+/*const toolState = {}; */
+
 const toolMenu = document.querySelector('.tool-menu');
 toolMenu.addEventListener('click', selectTool);
 
+const canvas = document.querySelector('.canvas');
+const transformation = document.querySelector('.transformation');
+function transform(event) {
+  let target = event.target;
+  if (transformation.classList.contains('selected-tool')) {
+    target.classList.toggle('transformation');
+  }
+}
 
+canvas.addEventListener('click', transform);
 
+/*
+document.addEventListener('click', function(event) {
+  let target = event.target;
+  const figures = [...canvas.children];
+  if (figures.every(() => i !== target)) {
+
+  }
+})
+*/
 
